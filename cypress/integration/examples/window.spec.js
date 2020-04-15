5b57980a07795b7e8d53f279bf4102805a9bfc1c
kolesan <kolesan90@gmail.com>
2020-04-15 15:49:26 +0300

    add more specs

--- cypress/integration/examples/window.spec.js
+++ cypress/integration/examples/window.spec.js
@@ -1,9 +1,19 @@
 describe('Intersection observer', () => {
   beforeEach(() => {
-    cy.visit('https://cypress-repro.netlify.com/')
+    cy.visit('https://cypress-repro.netlify.com/');
   })
 
   it('detects intersection and shows an alert', () => {
-    cy.get("#target").log();
+    cy.on('window:alert', str => {
+      expect(str).to.equal(`Found it!`);
+    });
+    cy.get("#target").scrollIntoView();
+  })
+
+  it('does not detect intersection', () => {
+    cy.on('window:alert', str => {
+      expect(str).to.equal(`not the string you are looking for`);
+    });
+    cy.get("#target");
   })
 })
