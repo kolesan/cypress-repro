5d9d18c8a96bddcf5bcdefb34b01dea8520c36c4
kolesan <kolesan90@gmail.com>
2020-04-15 15:29:29 +0300

    reduce height to 0

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const [red, blue] = body.querySelectorAll("div");
 
 new IntersectionObserver(([intersection]) => {
     if (intersection.isIntersecting) {
-        console.log("Found you!");
+        console.log("Found it!");
+        alert("Found it!");
     }
 }).observe(blue);
